47ecba381fbe45878a71ba642d5ac953cfe658c4
Elshaman <cbuitragoo.2023@gmail.com>
2023-08-07 19:25:34 -0500

    login y dependencias

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+
 
 class Cliente(db.Model):
     __tablename__ = "clientes"
@@ -25,6 +31,7 @@ class Venta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.id'))
+    detalles = db.relationship('DetalleVenta' ,  backref= 'venta' , lazy='dynamic' )
 
     def __repr__(self):
         return '<Venta {}>'.format(self.id)
@@ -32,9 +39,13 @@ class Venta(db.Model):
 class Producto(db.Model):
     __tablename__ = "productos"
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(64), index=True, unique=True)
+    nombre = db.Column(db.String(64), index=True, unique=False)
     precio = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
-    imagen = db.Column(db.String(120), index=True, nullable = False)
+    imagen = db.Column(db.String(120), index=True, nullable = True)
+    detalles = db.relationship('DetalleVenta', backref='producto', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Producto {}>'.format(self.nombre )
 
 class DetalleVenta(db.Model):
     __tablename__ = "detalles"
@@ -42,6 +53,23 @@ class DetalleVenta(db.Model):
     producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'))
     venta_id = db.Column(db.Integer, db.ForeignKey('ventas.id'))
 
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            c = Cliente(username = form.username.data , email = form.email.data , password_hash = form.password.data)
+            db.session.add(c)
+            db.session.commit()
+            return "hecho"
+        return render_template('login.html', form=form)
 
 ##from models import Cliente
 
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config(object):
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/flask-shoppy'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:admin@localhost:3310/flask-shopy'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'you-will-never-guess'
